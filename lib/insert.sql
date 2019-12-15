720305d5bfc52786adb0b2346cec5835897dc11d
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-12-15 02:21:21 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+('Mr. Chocolate', 1,'m','brown', 'tame', 1),
+('Rowdy', 1,'m','brown', 'tame', 1),
+('Tabitha', 1,'m','brown', 'tame', 1),
+('Sergeant Brown', 1,'m','brown', 'tame', 1),
+('Melissa', 1,'m','brown', 'tame', 1),
+('Grinch', 1,'m','brown', 'tame', 1),
+('Wendy', 1,'m','brown', 'tame', 1),
+(NULL, 1,'m','brown', 'tame', 1)
