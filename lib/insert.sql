e284769582259e104de0001e9205b5650849e9d8
Justin Miller <jayman1294@gmail.com>
2019-12-23 09:11:51 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Mr.Chocolate", 10, "M", "Brown", "Funny", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Rowdy", 8, "M", "Brown", "Funny", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Tabitha", 8, "F", "Brown", "Funny", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Sergeant Brown", 12, "M", "Brown", "Funny", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Melissa", 9, "F", "Brown", "Funny", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Grinch", 2, "M", "Brown", "Funny", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Wendy", 6, "F", "Brown", "Funny", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES(NULL, 11, "Male", "Brown", "Funny", 1);
